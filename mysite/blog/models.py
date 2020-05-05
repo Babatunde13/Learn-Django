f6f41016c4245500c3904223a41717d01ff62f8b
Babatunde13 <koikibabatunde14@gmail.com>
2020-05-05 22:48:36 +0100

    Added filters to the custom manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                    self).get_queryset().filter(
+                        status='published')
+
 class Post(models.Model):
     STATUS_CHOICES=(
         ('draft', 'Draft'),
@@ -26,9 +32,15 @@ class Post(models.Model):
                             choices=STATUS_CHOICES,
                             default='draft'
     )
+    objects=models.Manager() # The default manager
+    published=PublishedManager() # Our custom manager
 
     class Meta:
         ordering = ('-publish', )
 
     def __str__(self):
         return self.title
+
+
+        
+
